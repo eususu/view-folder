3ca4e79deccedc8a15d8d32df60e0ed7d3e14b55
jongjin pak <belovian@gmail.com>
2022-07-25 01:32:25 +0900

    add dir.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,87 @@
-use std::path::Path;
+use std::path::{Path, Component};
 use std::path::PathBuf;
 use std::os::unix::fs;
 
 
 struct View {
-	dest_base: String
+	dest_base: String,
 }
 
 impl View {
 	pub fn new(dest_base: String) -> Self {
 		Self {
-			dest_base,
+			dest_base
 		}
+	}
+
+	fn trace_directory(&self, _dest: & mut PathBuf, _dir: & PathBuf) -> std::io::Result<()> {
+
+		println!("BEGINO TRACE");
+
+		let mut dest = PathBuf::from(_dest.as_path());
+
+
+		let mut list = _dir.components();
+		let is_dir = _dir.is_dir();
+		let is_file = _dir.is_file();
+
+		let mut filename = "";
+		if is_file {
+			filename = _dir.file_name().unwrap().to_str().unwrap();
+		};
+
+		// bypass root dir
+		list.next();
+
+		for p in list {
+
+			if is_file {
+					println!("FILE FILE  {:?} == {:?}", dest, filename);
+				if p.as_os_str() == filename {
+				dest.push(p);
+					dest.set_file_name(&filename);
+					println!("FILENAME");
+					break;
+				}
+			}
+			dest.push(p);
+
+			if !dest.exists() {
+				println!("MKDIR {:?}", dest);
+				std::fs::create_dir(&dest);
+			}
+		}
+		println!("MIDDLE {:?}", dest);
+
+		if is_file {
+			println!("LINK {:?} ============ {:?}", dest, _dir);
+			let cdir = std::fs::canonicalize(&_dir)?;
+			fs::symlink(&cdir, dest);
+		}
+
+
+
+
+
+		Ok(())
 	}
 
 	pub fn add_directory(&self, _dir: &str) -> std::io::Result<()> {
 		// trace directory
 		let base_path = Path::new(_dir);
 
-		println!(">>>>>>>>>> {:?}", base_path.to_str());
+		//println!(">>>>>>>>>> {:?}", base_path.to_str());
 
 		for entry in base_path.read_dir().expect("read_dir failed") {
 			if let Ok(entry) = entry {
+				//println!(">> entry={:?}", entry.path());
+
+				let mut target = PathBuf::from(self.dest_base.to_string());
+
+				self.trace_directory(& mut target, &entry.path());
 				if entry.path().is_dir() {
 					self.add_directory(entry.path().to_str().unwrap());
-				} else {
-
-					println!("link >> {:?}", entry);
 				}
-
-
-				println!(">> entry={:?}", entry.path());
 			}
 		}
 		Ok(())
@@ -61,24 +112,16 @@ impl View {
 			Err(_error) => Ok(())
 		}
 	}
+
 }
 
 fn main() -> std::io::Result<()> {
 	let output = String::from("output");
-	let target1 = String::from("../example/test1.md");
-	let target2 = String::from("../example/test2.md");
-
-
-
 
 	let v = View::new(output);
+	println!("-------------------");
 	v.add_directory("example");
 
-	v.add_target(target1)?;
-	println!("-------------------");
-	v.add_target(target2)?;
-	
 
-	//let _ = fs::symlink(target1, output);
 	Ok(())
 }
